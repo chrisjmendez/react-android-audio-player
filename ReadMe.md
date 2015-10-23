3354c8ea70571fb9fe03d62bc823e94e9152ed0c
Chris Mendez <signup@chrisjmendez.com>
2015-10-22 17:53:17 -0700

    initial import

---
+++ ReadMe.md
@@ -0,0 +1,11 @@
+React Native Audio Player for Android
+=
+
+Installation
+- 
+1. [Setup guide](https://gist.github.com/chrisjmendez/e62b5a46323fc3c6e1f3)
+
+
+Resources
+-
+- [react-native-android-audio-streaming-aac](https://www.npmjs.com/package/react-native-android-audio-streaming-aac0)
---
+++ index.android.js
@@ -0,0 +1,95 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+var React = require('react-native');
+var RCTDeviceEventEmitter = require('RCTDeviceEventEmitter');
+var TouchableHighlight = require('TouchableHighlight');
+var Subscribable = require('Subscribable');
+var {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+} = React;
+
+var AACStreamingAndroid = require('react-native-android-audio-streaming-aac');
+
+var examplestreaming = React.createClass({
+  mixins: [Subscribable.Mixin],
+  getInitialState: function() {
+    return {
+      streaming: false,
+      eventName: 'none'
+    };
+  },
+  componentWillMount: function() {
+    this.addListenerOn(RCTDeviceEventEmitter, 'streamingOpen', (e: Event) => {
+      this.setState({
+        streaming: true
+      })
+    });
+    this.addListenerOn(RCTDeviceEventEmitter, 'streamingEvent', (e: Event) => {
+      this.setState({
+        eventName: e.eventName
+      })
+    });
+
+  },
+  onPressButtonPlay: function() {
+    AACStreamingAndroid.play();
+  },
+  onPressButtonURL: function() {
+    AACStreamingAndroid.setURLStreaming('http://4983.live.streamtheworld.com:80/KDFCFM_SC');
+  },
+  render: function() {
+    let html;
+    if(!this.state.streaming) {
+      html = (<TouchableHighlight onPress={this.onPressButtonURL}>
+        <Text style={styles.instructions}>
+          SET URL
+        </Text>
+      </TouchableHighlight>);
+    } else {
+      html = (<TouchableHighlight onPress={this.onPressButtonPlay}>
+        <Text style={styles.instructions}>
+          PLAY
+        </Text>
+      </TouchableHighlight>);
+    }
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+			React Native for Android!
+        </Text>
+        {html}
+        <Text style={styles.instructions}>
+          {this.state.eventName}
+        </Text>
+      </View>
+    );
+  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('example', () => examplestreaming);
